35ed60b2dfb6489c0dab6ccb9a2e6c44cf9b0620
leestephen0320 <leestephen0320@gmail.com>
2023-02-13 03:57:03 +0000

    modified:   src/main/java/ArraySum.java

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,10 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int a = 0;
+        for(int i = 0; i < arr.length; i++){
+            a = a + arr[i];
+        }
+        return a;
     }
 }
